67387e93cdf7463540367fd14bb2eb2a0f5b3f51
birkiy <umutberkayaltintas@gmail.com>
2025-10-28 18:27:14 -0700

    Remove conda recipe from version control

--- conda/recipe/bld.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Minimal Windows build script (may not be used on conda-forge if noarch: python)
-python -m pip install . --no-deps -vv
--- conda/recipe/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Build script for conda (unix)
-set -euo pipefail
-
-# Install the package into the build environment using pip.
-"${PYTHON}" -m pip install . --no-deps -vv
